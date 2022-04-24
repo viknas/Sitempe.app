5525e40d6c86ee488353a2592f205924a3bbdf50
viknas <viknasramadhan11@gmail.com>
2022-04-24 14:28:22 +0700

    Add fake admin account for testing purpose

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -17,5 +18,13 @@ class DatabaseSeeder extends Seeder
         \App\Models\District::factory(10)->create();
         \App\Models\Regency::factory(10)->create();
         \App\Models\User::factory(10)->create();
+
+        User::create([
+            'nama' => 'Super Admin',
+            'email' => 'admin@mail.com',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'id_kecamatan' => $this->faker->numberBetween(1, 9),
+            'id_kabupaten' => $this->faker->numberBetween(1, 9),
+        ]);
     }
 }
