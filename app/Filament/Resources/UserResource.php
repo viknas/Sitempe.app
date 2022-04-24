79647b7cb1bf0a0572206c3d767383aa8c716706
viknas <viknasramadhan11@gmail.com>
2022-04-24 12:46:41 +0700

    Remove ability to edit reseller data

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -140,7 +140,6 @@ class UserResource extends Resource
         return [
             'index' => Pages\ListUsers::route('/'),
             'create' => Pages\CreateUser::route('/create'),
-            'edit' => Pages\EditUser::route('/{record}/edit'),
         ];
     }
 }
