e32838f2f0b24364e25e6f6f8385ec1d49fb4fe6
viknas <viknasramadhan11@gmail.com>
2022-04-27 16:33:37 +0700

    Add stats for today's income and expense

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -2,18 +2,42 @@
 
 namespace App\Filament\Widgets;
 
+use Akaunting\Money\Money;
+use App\Models\Expense;
+use App\Models\Income;
+use Carbon\Carbon;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Card;
 
 class StatsOverview extends BaseWidget
 {
+    public string $todayIncome;
+    public string $todayExpense;
+    public string $monthIncome;
+    public string $monthExpense;
+
+    public function mount()
+    {
+
+        $todayIncome = Income::whereDate('tanggal', Carbon::now())
+            ->selectRaw("SUM(total_harga) AS total_harga, DATE(tanggal) as date")
+            ->groupBy('date')->get();
+
+        $todayExpense = Expense::whereDate('tanggal', Carbon::now())
+            ->selectRaw("SUM(jumlah_pengeluaran) AS jumlah_pengeluaran, DATE(tanggal) as date")
+            ->groupBy('date')->get();
+
+        $this->todayExpense = Money::IDR($todayExpense[0]['jumlah_pengeluaran']  ?? 0, true);
+        $this->todayIncome = Money::IDR($todayIncome[0]['total_harga'] ?? 0, true);
+    }
+
     protected function getCards(): array
     {
         return [
-            Card::make('Pendapatan hari ini', '13k'),
-            Card::make('Pengeluaran hari ini', '13k'),
-            Card::make('Pendapatan bulan ini ', '13k'),
-            Card::make('Pengeluaran bulan ini', '13k'),
+            Card::make('Pendapatan hari ini', $this->todayIncome),
+            Card::make('Pengeluaran hari ini', $this->todayExpense),
+            Card::make('Pendapatan bulan ini ', 'Rp0,00'),
+            Card::make('Pengeluaran bulan ini', 'Rp0,00'),
         ];
     }
 }
